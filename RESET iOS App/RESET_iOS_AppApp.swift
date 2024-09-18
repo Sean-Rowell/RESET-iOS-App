ddc40b74c6d928b4c436aaacc3e731e6f268a41f
Sean Rowell <88912963+Sean-Rowell@users.noreply.github.com>
2024-09-17 21:08:32 -0400

    Initial Commit

---
+++ RESET iOS App/RESET_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESET_iOS_AppApp.swift
+//  RESET iOS App
+//
+//  Created by Sean Rowell on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct RESET_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RESET iOS AppTests/RESET_iOS_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RESET_iOS_AppTests.swift
+//  RESET iOS AppTests
+//
+//  Created by Sean Rowell on 9/17/24.
+//
+
+import Testing
+@testable import RESET_iOS_App
+
+struct RESET_iOS_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
